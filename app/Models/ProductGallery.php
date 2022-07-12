0430b080e53227dd41dccbca1b526392fb2ea2c1
Adan Nugraha <contact.ardana@gmail.com>
2022-07-12 18:52:17 +0700

    first commit

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class ProductGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'products_id',
+        'url'
+    ];
+
+    // return real url
+    public function getUrlAttribute ($url)
+    {
+        return config('app.url') . Storage::url($url);
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'users_id',
+        'address',
+        'payment',
+        'total_price',
+        'shipping_price',
+        'status'
+    ];
+
+    public function users()
+    {
+        $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function items()
+    {
+        $this->hasMany(TransactionItem::class, 'transactions_id', 'id');
+    }
+}
