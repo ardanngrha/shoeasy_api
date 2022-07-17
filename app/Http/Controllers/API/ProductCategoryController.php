87d31de2b3223c6f4d3dc5d7908b96a46120d35e
Adan Nugraha <contact.ardana@gmail.com>
2022-07-17 22:31:56 +0700

    response formatter; api product, category

---
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $show_product = $request->input('show_product');
+
+        if ($id) {
+            $category = ProductCategory::with(['products'])->find($id);
+            if ($category) {
+                return ResponseFormatter::success(
+                    $category,
+                    'Data Kategori Berhasil Diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Produk Tidak Ada',
+                    404
+                );
+            }
+        }
+
+        $category = ProductCategory::query();
+
+        if ($name)
+        {
+            $category->where('name', 'like', '%' . $name . '%');
+        }
+
+        if ($show_product)
+        {
+            $category->with('products');
+        }
+
+        return ResponseFormatter::success(
+            $category->paginate($limit),
+            'Data List Kategori Berhasil Diambil'
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\ProductCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', [ProductController::class, 'all']);
+Route::get('categories', [ProductCategoryController::class, 'all']);
