cd7f2faef50bd77da4a1f8652a507bf67ba1c426
Adan Nugraha <contact.ardana@gmail.com>
2022-08-16 12:17:55 +0700

    setup laravel datatables

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductGallery extends Model
 {
@@ -20,6 +20,7 @@ class ProductGallery extends Model
     public function getUrlAttribute($url)
     {
         // Your IPv4 Address
-        return 'http://192.168.0.101/shoeasy_backend/public'. Storage::url($url);
+        return 'http://20.20.23.170/shoeasy_backend/public' . Storage::url($url);
+        // return 'http://192.168.0.101/shoeasy_backend/public' . Storage::url($url);
     }
 }
